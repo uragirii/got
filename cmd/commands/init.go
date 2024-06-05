3b68c497b9ad33f350e31ee88d1f062d7f5c845f
Apoorv Kansal <apoorvkansalak@gmail.com>
2024-06-05 23:02:04 +0530

    feat: basic setup

---
+++ cmd/commands/init.go
@@ -0,0 +1,7 @@
+package commands
+
+import "fmt"
+
+func Init() {
+	fmt.Println("init function called")
+}
--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,36 @@ package main
 import (
 	"fmt"
 	"os"
+	"slices"
+
+	"github.com/uragirii/got/cmd/commands"
 )
 
+var COMMAND_TO_FUNC = map[string]func(){
+	"init": commands.Init,
+}
+
+var SUPPORTED_COMMANDS = []string{"init"}
+
 func main() {
-	fmt.Println(os.Args)
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		fmt.Println("no arguments were provided")
+		return
+	}
+
+	command := args[0]
+
+	isValidCmd := slices.Contains(SUPPORTED_COMMANDS, command)
+
+	if !isValidCmd {
+		fmt.Printf("%s is not a supported argument\n", command)
+		return
+	}
+
+	commandFunc := COMMAND_TO_FUNC[command]
+
+	commandFunc()
+
 }
