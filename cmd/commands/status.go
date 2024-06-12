811da92d544571038d550af33c53859ce0342dd2
Apoorv Kansal <apoorvkansalak@gmail.com>
2024-06-13 02:35:29 +0530

    feat: temp

--- cmd/commands/status.go
+++ cmd/commands/status.go
@@ -2,10 +2,7 @@ package commands
 
 import (
 	"fmt"
-	"os"
 	"path"
-	"path/filepath"
-	"sync"
 
 	"github.com/uragirii/got/cmd/internals"
 )
@@ -19,57 +16,57 @@ var STATUS *internals.Command = &internals.Command{
 
 // TODO: extract to internals as might be needed
 // Crawls recursively and returns all the files that should be under git's supervision
-func crawlDir(loc string, filesChan chan<- string, wg *sync.WaitGroup, gitIgnore *internals.GitIgnore) {
+// func crawlDir(loc string, filesChan chan<- string, wg *sync.WaitGroup, gitIgnore *internals.GitIgnore) {
 
-	items, err := os.ReadDir(loc)
+// 	items, err := os.ReadDir(loc)
 
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
 
-	dirs := make([]string, 0, len(items))
-	files := make([]string, 0, len(items))
+// 	dirs := make([]string, 0, len(items))
+// 	files := make([]string, 0, len(items))
 
-	for _, dirItem := range items {
-		if dirItem.IsDir() {
-			if dirItem.Name() != ".git" {
-				dirs = append(dirs, path.Join(loc, dirItem.Name()))
-			}
+// 	for _, dirItem := range items {
+// 		if dirItem.IsDir() {
+// 			if dirItem.Name() != ".git" {
+// 				dirs = append(dirs, path.Join(loc, dirItem.Name()))
+// 			}
 
-		} else {
+// 		} else {
 
-			filePath := path.Join(loc, dirItem.Name())
+// 			filePath := path.Join(loc, dirItem.Name())
 
-			files = append(files, filePath)
+// 			files = append(files, filePath)
 
-			if dirItem.Name() == ".gitignore" {
-				gitIgnore, err = gitIgnore.WithFile(filePath, loc)
+// 			if dirItem.Name() == ".gitignore" {
+// 				gitIgnore, err = gitIgnore.WithFile(filePath, loc)
 
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-			}
-		}
-	}
+// 				if err != nil {
+// 					fmt.Println(err)
+// 					return
+// 				}
+// 			}
+// 		}
+// 	}
 
-	for _, file := range files {
-		if !gitIgnore.Match(file) {
-			filesChan <- file
-		}
-	}
+// 	for _, file := range files {
+// 		if !gitIgnore.Match(file) {
+// 			filesChan <- file
+// 		}
+// 	}
 
-	for _, dir := range dirs {
-		if !gitIgnore.Match(dir) {
-			wg.Add(1)
-			go func(dirpath string) {
-				defer wg.Done()
-				crawlDir(dirpath, filesChan, wg, gitIgnore)
-			}(dir)
-		}
-	}
-}
+// 	for _, dir := range dirs {
+// 		if !gitIgnore.Match(dir) {
+// 			wg.Add(1)
+// 			go func(dirpath string) {
+// 				defer wg.Done()
+// 				crawlDir(dirpath, filesChan, wg, gitIgnore)
+// 			}(dir)
+// 		}
+// 	}
+// }
 
 func Status(c *internals.Command, gitPath string) {
 
@@ -78,78 +75,80 @@ func Status(c *internals.Command, gitPath string) {
 		return
 	}
 
+	fmt.Println(internals.GetTreeHash(path.Join(gitPath, "..")))
+
 	// This 10 would also force to only run 10 goroutines at a time (Hopefully)
-	filesChan := make(chan string, 10)
-	rootDir := path.Join(gitPath, "..")
+	// filesChan := make(chan string, 10)
+	// rootDir := path.Join(gitPath, "..")
 
-	var gitIndex internals.GitIndex
+	// var gitIndex internals.GitIndex
 
-	err := gitIndex.New(gitPath)
+	// err := gitIndex.New(gitPath)
 
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
 
-	possibleNumFiles := len(gitIndex.GetTrackedFiles())
+	// possibleNumFiles := len(gitIndex.GetTrackedFiles())
 
-	files := make([]string, 0, possibleNumFiles)
+	// files := make([]string, 0, possibleNumFiles)
 
-	var wg sync.WaitGroup
+	// var wg sync.WaitGroup
 
-	var gitIgnore internals.GitIgnore
+	// var gitIgnore internals.GitIgnore
 
-	crawlDir(rootDir, filesChan, &wg, &gitIgnore)
+	// crawlDir(rootDir, filesChan, &wg, &gitIgnore)
 
-	go func() {
-		wg.Wait()
-		close(filesChan)
-	}()
+	// go func() {
+	// 	wg.Wait()
+	// 	close(filesChan)
+	// }()
 
-	for file := range filesChan {
-		relPath, err := filepath.Rel(rootDir, file)
-		if err != nil {
-			panic(err)
-		}
-		files = append(files, relPath)
-	}
+	// for file := range filesChan {
+	// 	relPath, err := filepath.Rel(rootDir, file)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	files = append(files, relPath)
+	// }
 
-	created := make([]string, 0, 10)
-	changeChan := make(chan string, 10)
+	// created := make([]string, 0, 10)
+	// changeChan := make(chan string, 10)
 
-	var fwg sync.WaitGroup
+	// var fwg sync.WaitGroup
 
-	for _, file := range files {
+	// for _, file := range files {
 
-		if gitIndex.Has(file) {
-			fwg.Add(1)
-			go func(file string) {
-				defer fwg.Done()
-				sha, _, err := internals.HashBlob(file, false)
-				if err != nil {
-					fmt.Println(err)
-				}
+	// 	if gitIndex.Has(file) {
+	// 		fwg.Add(1)
+	// 		go func(file string) {
+	// 			defer fwg.Done()
+	// 			sha, _, err := internals.HashBlob(file, false)
+	// 			if err != nil {
+	// 				fmt.Println(err)
+	// 			}
 
-				if string(sha[:]) != string((*gitIndex.Get(file)).SHA1[:]) {
-					changeChan <- file
-				}
+	// 			if string(sha[:]) != string((*gitIndex.Get(file)).SHA1[:]) {
+	// 				changeChan <- file
+	// 			}
 
-			}(file)
-		} else {
-			created = append(created, file)
-		}
-	}
-	go func() {
-		fwg.Wait()
-		close(changeChan)
-	}()
+	// 		}(file)
+	// 	} else {
+	// 		created = append(created, file)
+	// 	}
+	// }
+	// go func() {
+	// 	fwg.Wait()
+	// 	close(changeChan)
+	// }()
 
-	for changedF := range changeChan {
-		fmt.Println("Changed", changedF)
-	}
+	// for changedF := range changeChan {
+	// 	fmt.Println("Changed", changedF)
+	// }
 
-	for _, newF := range created {
-		fmt.Println("Created", newF)
-	}
+	// for _, newF := range created {
+	// 	fmt.Println("Created", newF)
+	// }
 
 }
