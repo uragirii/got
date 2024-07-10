ae535592eb66c9e14b5c84aa1d3112bd73cf6ca0
Apoorv Kansal <apoorvkansalak@gmail.com>
2024-07-10 23:02:29 +0530

    feat: add error

--- internals/git/index/errors.go
+++ internals/git/index/errors.go
@@ -5,3 +5,5 @@ import "fmt"
 var ErrInvalidIndex = fmt.Errorf("invalid index file")
 var ErrVersionNotSupported = fmt.Errorf("index file version not supported")
 var ErrCorruptedIndex = fmt.Errorf("index file corrupted")
+
+var ErrInvalidEntryMode = fmt.Errorf("entry mode is invalid")
