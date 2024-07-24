ed2bb1612ba9c33c8f791c213fc70e1a6c8cf3a1
Apoorv Kansal <apoorvkansalak@gmail.com>
2024-07-24 23:40:56 +0530

    refactor: make object as an interface and redo the tests

--- internals/git/object/git_object.go
+++ internals/git/object/git_object.go
@@ -1,134 +1,113 @@
 package object
 
 import (
-	"bytes"
 	"compress/zlib"
-	"errors"
 	"fmt"
 	"io"
-	"io/fs"
-	"os"
+	"slices"
+	"strconv"
 
 	"github.com/uragirii/got/internals/git/sha"
 )
 
 var ErrInvalidObj = fmt.Errorf("invalid git object")
 
-type Object struct {
-	// Unmarshall(path string)
-	// GetObjType() ObjectType
-	// PrettyPrint()
-	objectType ObjectType
-	// Also contains header for the object
-	uncompressedContents *[]byte
-	sha                  *sha.SHA
+const BlobHeader string = "blob %d\u0000"
+const TreeHeader string = "tree %d\u0000"
+const CommitHeader string = "commit %d\u0000"
+
+type ObjectType string
+
+const (
+	BlobObj   ObjectType = "blob"
+	TreeObj   ObjectType = "tree"
+	CommitObj ObjectType = "commit"
+)
+
+type Object interface {
+	GetSHA() *sha.SHA
+	Write(io.Writer) error
+	GetObjType() ObjectType
+	// Pretty print the object
+	String() string
+	Read(io.Reader)
 }
 
-func FromSHA(sha *sha.SHA, fs fs.FS) (*Object, error) {
-	objPath, err := sha.GetObjPath()
+type ObjectContents struct {
+	ObjType ObjectType
+	// These are decompressed contents
+	Contents *[]byte
+}
+
+func Decompress(reader io.Reader) (*[]byte, error) {
+	reader, err := zlib.NewReader(reader)
 
 	if err != nil {
 		return nil, err
 	}
 
-	objFile, err := fs.Open(objPath)
+	uncompressed, err := io.ReadAll(reader)
 
 	if err != nil {
 		return nil, err
 	}
 
-	defer objFile.Close()
-
-	decompressedContents, err := Decompress(objFile)
-
-	if err != nil {
-		return nil, err
-	}
-
-	objType, err := GetType(decompressedContents)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return &Object{
-		sha:                  sha,
-		objectType:           objType,
-		uncompressedContents: decompressedContents,
-	}, nil
+	return &uncompressed, nil
 }
 
-func (obj *Object) RawString() string {
-	return fmt.Sprint(string(*(obj.getContentWithoutHeader())))
-}
+func GetContents(decompressedContents *[]byte) (ObjectContents, error) {
+	headerEndIdx := slices.Index(*decompressedContents, 0x00)
 
-func (obj *Object) GetObjType() ObjectType {
-	return obj.objectType
-}
+	if headerEndIdx == -1 {
+		return ObjectContents{}, ErrInvalidObj
+	}
 
-func (obj *Object) Write(w io.Writer) error {
+	header := (*decompressedContents)[:headerEndIdx]
+	contents := (*decompressedContents)[headerEndIdx+1:]
 
-	writer := zlib.NewWriter(w)
+	headerSpaceIdx := slices.Index(header, 0x20) // SPACE
 
-	_, err := writer.Write(*obj.uncompressedContents)
+	if headerSpaceIdx == -1 {
+		return ObjectContents{}, ErrInvalidObj
+	}
+
+	byteLenStr := string((*decompressedContents)[headerSpaceIdx+1 : headerEndIdx])
+
+	byteLen, err := strconv.Atoi(byteLenStr)
 
 	if err != nil {
-		return err
+		fmt.Println("invalid bytelen", err)
+		return ObjectContents{}, ErrInvalidObj
 	}
 
-	writer.Close()
-
-	return nil
-}
-
-func (obj *Object) WriteToFile() error {
-	objPath, err := obj.sha.GetObjPath()
-
-	if err != nil {
-		return err
+	if byteLen != len(contents) {
+		fmt.Printf("expected bytelen to be %d but found %d\n", byteLen, len(contents))
+		return ObjectContents{}, ErrInvalidObj
 	}
 
-	if _, err := os.Stat(objPath); errors.Is(err, os.ErrNotExist) {
+	objType := string(header[:headerSpaceIdx])
 
-		var compressBytes bytes.Buffer
+	switch ObjectType(objType) {
+	case BlobObj:
+		return ObjectContents{
+			ObjType:  BlobObj,
+			Contents: &contents,
+		}, nil
 
-		err := obj.Write(&compressBytes)
+	case TreeObj:
+		return ObjectContents{
+			ObjType:  TreeObj,
+			Contents: &contents,
+		}, nil
 
-		if err != nil {
-			return err
-		}
+	case CommitObj:
+		return ObjectContents{
+			ObjType:  CommitObj,
+			Contents: &contents,
+		}, nil
 
-		return os.WriteFile(objPath, compressBytes.Bytes(), 0444)
+	default:
+		return ObjectContents{}, ErrInvalidObj
+
 	}
-	return nil
-}
-
-func (obj *Object) GetSHA() *sha.SHA {
-	return obj.sha
-}
-
-// Creates a new object from the raw file
-// It doesn't read the existing object, instead hashes a file
-func FromFile(reader io.Reader) (*Object, error) {
-	data, err := io.ReadAll(reader)
-
-	if err != nil {
-		return nil, err
-	}
-
-	header := []byte(fmt.Sprintf(_GitBlobHeader, len(data)))
-
-	contents := append(header, data...)
-
-	sha, err := sha.FromData(&contents)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return &Object{
-		objectType:           BlobObj,
-		uncompressedContents: &contents,
-		sha:                  sha,
-	}, nil
 }
--- internals/git/object/git_objects_test.go
+++ internals/git/object/git_objects_test.go
@@ -1,91 +1,87 @@
 package object_test
 
 import (
-	"bytes"
-	"strings"
+	"fmt"
 	"testing"
-	"testing/fstest"
 
 	"github.com/uragirii/got/internals/git/object"
-	"github.com/uragirii/got/internals/git/sha"
 )
 
-func TestObject(t *testing.T) {
+func TestGetContents(t *testing.T) {
+	DATA := "data data data data"
+	DATA_LEN := len(DATA)
 
-	// Example is taken from https://git-scm.com/book/en/v2/Git-Internals-Git-Objects
-	TEST_STR := "what is up, doc?"
-	DATA := strings.NewReader(TEST_STR)
-	DATA_SHA_STR := "bd9dbf5aae1a3862dd1526723246b20206e5fc37"
-	DATA_COMPRESSED_BYTES := []byte{0x78, 0x9c, 0x4a, 0xca, 0xc9, 0x4f, 0x52, 0x30, 0x34, 0x63, 0x28, 0xcf, 0x48, 0x2c, 0x51, 0xc8,
-		0x2c, 0x56, 0x28, 0x2d, 0xd0, 0x51, 0x48, 0xc9, 0x4f, 0xb6, 0x07, 0x04, 0x00, 0x00, 0xff, 0xff,
-		0x5f, 0x1c, 0x07, 0x9d}
+	t.Run("parses blob obj correctly", func(t *testing.T) {
+		blobHeader := fmt.Sprintf(object.BlobHeader, DATA_LEN)
+		uncompressedObj := []byte(fmt.Sprintf("%s%s", blobHeader, DATA))
 
-	t.Run("hashes and reads the file correctly", func(t *testing.T) {
-
-		obj, err := object.FromFile(DATA)
+		contents, err := object.GetContents(&uncompressedObj)
 
 		if err != nil {
-			t.Error(err)
+			t.Errorf("expected err to be nil but found %v", err)
 		}
 
-		if obj.GetSHA().MarshallToStr() != DATA_SHA_STR {
-			t.Errorf("expected the sha to be %s but got %s", DATA_SHA_STR, obj.GetSHA().MarshallToStr())
+		if contents.ObjType != object.BlobObj {
+			t.Errorf("expected BlobObj but found %s", contents.ObjType)
 		}
 
-		var objFile bytes.Buffer
-
-		obj.Write(&objFile)
-
-		objBytes := objFile.Bytes()
-
-		if !bytes.Equal(objBytes, DATA_COMPRESSED_BYTES) {
-			t.Errorf("compressed data not equal to expected data")
-			t.Errorf("expected % x", DATA_COMPRESSED_BYTES)
-			t.Errorf("got % x", objBytes)
-		}
-
-		if obj.RawString() != TEST_STR {
-			t.Errorf("expected raw string to be %s but got %s", TEST_STR, obj.RawString())
-		}
-
-		if obj.GetObjType() != object.BlobObj {
-			t.Errorf("expected object type to be %s but got %s", object.BlobObj, obj.GetObjType())
+		if string(*contents.Contents) != DATA {
+			t.Errorf("expected data to be %s but got %s", DATA, *contents.Contents)
 		}
 
 	})
 
-	t.Run("reads data correctly from hashed object", func(t *testing.T) {
+	t.Run("parses commit obj correctly", func(t *testing.T) {
+		commitHeader := fmt.Sprintf(object.CommitHeader, DATA_LEN)
+		uncompressedObj := []byte(fmt.Sprintf("%s%s", commitHeader, DATA))
 
-		t.Setenv("GIT_DIR", ".git")
-
-		objSha, _ := sha.FromString(DATA_SHA_STR)
-
-		mapFs := fstest.MapFS{}
-
-		objPath, _ := objSha.GetObjPath()
-
-		mapFs[objPath] = &fstest.MapFile{Data: DATA_COMPRESSED_BYTES}
-
-		fs := fstest.MapFS(mapFs)
-
-		obj, err := object.FromSHA(objSha, fs)
+		contents, err := object.GetContents(&uncompressedObj)
 
 		if err != nil {
-			t.Error(err)
+			t.Errorf("expected err to be nil but found %v", err)
 		}
 
-		if !obj.GetSHA().Eq(objSha) {
-			t.Errorf("expected the sha to be %s but got %s", DATA_SHA_STR, obj.GetSHA().MarshallToStr())
+		if contents.ObjType != object.CommitObj {
+			t.Errorf("expected CommitObj but found %s", contents.ObjType)
 		}
 
-		if obj.RawString() != TEST_STR {
-			t.Errorf("expected raw string to be %s but got %s", TEST_STR, obj.RawString())
-		}
-
-		if obj.GetObjType() != object.BlobObj {
-			t.Errorf("expected object type to be %s but got %s", object.BlobObj, obj.GetObjType())
+		if string(*contents.Contents) != DATA {
+			t.Errorf("expected data to be %s but got %s", DATA, *contents.Contents)
 		}
 
 	})
 
+	t.Run("parses tree obj correctly", func(t *testing.T) {
+		treeHeader := fmt.Sprintf(object.TreeHeader, DATA_LEN)
+		uncompressedObj := []byte(fmt.Sprintf("%s%s", treeHeader, DATA))
+
+		contents, err := object.GetContents(&uncompressedObj)
+
+		if err != nil {
+			t.Errorf("expected err to be nil but found %v", err)
+		}
+
+		if contents.ObjType != object.TreeObj {
+			t.Errorf("expected TreeObj but found %s", contents.ObjType)
+		}
+
+		if string(*contents.Contents) != DATA {
+			t.Errorf("expected data to be %s but got %s", DATA, *contents.Contents)
+		}
+
+		t.Run("throws error when content len doesnt match", func(t *testing.T) {
+			blobHeader := fmt.Sprintf(object.BlobHeader, DATA_LEN+5)
+			uncompressedObj := []byte(fmt.Sprintf("%s%s", blobHeader, DATA))
+
+			_, err := object.GetContents(&uncompressedObj)
+
+			if err != nil {
+				return
+			} else {
+				t.Errorf("expected error to be thrown")
+			}
+
+		})
+
+	})
 }
